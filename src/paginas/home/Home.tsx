017e0751caa3a0d8299c8a3c4c61b9fc7e459f5a
tholivera <emaildathaisoliveira.silva@gmail.com>
2021-11-26 15:52:25 -0300

    Criação do blog pessoal com sua home

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import"./Home.css";
+
+function Home() {
+    return(
+        <>
+<h1 className="titulo">Home</h1>
+<img src="https://st2.depositphotos.com/1033604/9146/i/600/depositphotos_91467558-stock-photo-the-perfect-christmas-background.jpg" alt="Imagem tela inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;

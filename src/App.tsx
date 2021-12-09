14e4ba39ae23eb70387329bb506fdf9450984db1
tholivera <emaildathaisoliveira.silva@gmail.com>
2021-12-09 02:45:22 -0300

    Adicionando cadastro de usuário

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,37 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./paginas/home/Home";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
-import Login from "./paginas/login/Login";
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
 import './App.css';
+
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Switch>
-        <div style = {{minHeight: "100vh"}}>
-          
-        <Route exact path="/">
-            <Login />
-          </Route>
+        <Switch>
+          <div style={{minHeight: '100vh'}}>
 
-          <Route path="/login">
-            <Login />
-          </Route>
+            <Route exact path='/'>
+              <Login />
+            </Route>
 
-          <Route path="/home">
-            <Home />
-          </Route>
-        </div>
-      </Switch>
+            <Route path='/login'>
+              <Login />
+            </Route>
+
+            <Route path='/home'>
+              <Home />
+            </Route>
+            
+            <Route path='/cadastrousuario'>
+              <CadastroUsuario />
+            </Route>
+          </div>
+        </Switch>
       <Footer />
     </Router>
   );

8aa620709a3426a4748e6529628c2c15e1d09a2d
tholivera <emaildathaisoliveira.silva@gmail.com>
2021-12-09 01:10:12 -0300

    Adicionando Footer, Navbar, Home, Login e estilizando

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./paginas/home/Home";
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+import Login from "./paginas/login/Login";
 import './App.css';
 
 function App() {
   return (
-    <Home/>
+    <Router>
+      <Navbar />
+      <Switch>
+        <div style = {{minHeight: "100vh"}}>
+          
+        <Route exact path="/">
+            <Login />
+          </Route>
+
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/home">
+            <Home />
+          </Route>
+        </div>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
